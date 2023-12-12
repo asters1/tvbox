563263170e612b123be886558f75e3f13bac4cd8
asters1 <gj744559140@163.com>
2023-12-12 06:08:43 +0000

    修改

--- main.go
+++ main.go
@@ -10,6 +10,19 @@ import (
 	"github.com/go-yaml/yaml"
 )
 
+var (
+	extend              string
+	filter_switch       bool
+	filter_type_index   int
+	filter_num_index    int
+	search_switch       bool
+	search_keyword      string
+	test_type_index     int
+	test_category_page  string
+	test_vod_index      int
+	test_vod_from_index int
+)
+
 // 检查python环境
 func Go_Init() string {
 	M := make(map[string]interface{})
@@ -32,14 +45,15 @@ func Go_Init() string {
 	}
 }
 
-func Go_HomeContent(filter bool, file_name string) string {
+func Go_HomeContent(etd string, filter bool, file_name string) string {
 	M := make(map[string]interface{})
 	flr := "False"
 	if filter {
 		flr = "True"
 	}
 	File_Name_Remove_py := strings.TrimSuffix(file_name, ".py")
-	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import homeContent;homeContent("+flr+")")
+	fmt.Println("python3", "-c", "from "+File_Name_Remove_py+" import homeContent,init;init(\""+etd+"\");homeContent("+flr+")")
+	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import homeContent,init;init(\""+etd+"\");homeContent("+flr+")")
 	content, err := cmd.Output()
 	if err != nil {
 		M["code"] = 0
@@ -59,14 +73,14 @@ func Go_HomeContent(filter bool, file_name string) string {
 	}
 }
 
-func Go_CategoryContent(tid string, pg string, filter bool, extend string, file_name string) string {
+func Go_CategoryContent(etd string, tid string, pg string, filter bool, extend string, file_name string) string {
 	M := make(map[string]interface{})
 	flr := "False"
 	if filter {
 		flr = "True"
 	}
 	File_Name_Remove_py := strings.TrimSuffix(file_name, ".py")
-	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import categoryContent;categoryContent(\""+tid+"\",\""+pg+"\","+flr+",\""+extend+"\")")
+	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import categoryContent,init;init(\""+etd+"\");;categoryContent(\""+tid+"\",\""+pg+"\","+flr+",\""+extend+"\")")
 	content, err := cmd.Output()
 	if err != nil {
 		M["code"] = 0
@@ -86,10 +100,10 @@ func Go_CategoryContent(tid string, pg string, filter bool, extend string, file_
 	}
 }
 
-func Go_DetailContent(ids string, file_name string) string {
+func Go_DetailContent(etd string, ids string, file_name string) string {
 	M := make(map[string]interface{})
 	File_Name_Remove_py := strings.TrimSuffix(file_name, ".py")
-	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import detailContent;detailContent(\""+ids+"\")")
+	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import detailContent,init;init(\""+etd+"\");;detailContent(\""+ids+"\")")
 	content, err := cmd.Output()
 	if err != nil {
 		M["code"] = 0
@@ -109,10 +123,10 @@ func Go_DetailContent(ids string, file_name string) string {
 	}
 }
 
-func Go_SearchContent(key string, file_name string) string {
+func Go_SearchContent(etd string, key string, file_name string) string {
 	M := make(map[string]interface{})
 	File_Name_Remove_py := strings.TrimSuffix(file_name, ".py")
-	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import searchContent;searchContent(\""+key+"\")")
+	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import searchContent,init;init(\""+etd+"\");;searchContent(\""+key+"\")")
 	content, err := cmd.Output()
 	if err != nil {
 		M["code"] = 0
@@ -132,10 +146,10 @@ func Go_SearchContent(key string, file_name string) string {
 	}
 }
 
-func Go_PlayerContent(flag string, id string, file_name string) string {
+func Go_PlayerContent(etd string, flag string, id string, file_name string) string {
 	M := make(map[string]interface{})
 	File_Name_Remove_py := strings.TrimSuffix(file_name, ".py")
-	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import playerContent;playerContent(\""+flag+"\",\""+id+"\")")
+	cmd := exec.Command("python3", "-c", "from "+File_Name_Remove_py+" import playerContent,init;init(\""+etd+"\");;playerContent(\""+flag+"\",\""+id+"\")")
 	content, err := cmd.Output()
 	if err != nil {
 		M["code"] = 0
@@ -156,7 +170,7 @@ func Go_PlayerContent(flag string, id string, file_name string) string {
 }
 
 // 读取yaml配置文件
-func ReadYaml() {
+func InitConfig() {
 	data, err := ioutil.ReadFile("./config.yaml")
 	if err != nil {
 		fmt.Println("读取config.yaml文件失败!请检查!")
@@ -167,8 +181,75 @@ func ReadYaml() {
 	if err != nil {
 		fmt.Println("解析config.yaml文件失败!请检查!")
 	}
+	for k, v := range obj {
+		switch {
+		case k == "extend":
+			if str, ok := v.(string); ok {
+				extend = str
+			} else {
+				fmt.Println("extend不是字符串类型!请检查!")
+			}
+		case k == "filter_switch":
+			if bl, ok := v.(bool); ok {
+				filter_switch = bl
+			} else {
+				fmt.Println("filter_switch不是bool类型!请检查!")
+			}
+		case k == "filter_type_index":
+			if num, ok := v.(int); ok {
+				filter_type_index = num
+			} else {
+				fmt.Println("filter_type_index不是int类型!请检查!")
+			}
+		case k == "filter_num_index":
+			if num, ok := v.(int); ok {
+				filter_num_index = num
+			} else {
+				fmt.Println("filter_num_index不是int类型!请检查!")
+			}
+		case k == "search_switch":
+			if bl, ok := v.(bool); ok {
+				search_switch = bl
+			} else {
+				fmt.Println("search_switch不是bool类型!请检查!")
+			}
+		case k == "search_keyword":
+			if str, ok := v.(string); ok {
+				search_keyword = str
+			} else {
+				fmt.Println("search_keyword不是字符串类型!请检查!")
+			}
+		case k == "test_type_index":
+			if num, ok := v.(int); ok {
+				test_type_index = num
+			} else {
+				fmt.Println("test_type_index不是int类型!请检查!")
+			}
+		case k == "test_category_page":
+			if str, ok := v.(string); ok {
+				test_category_page = str
+			} else {
+				fmt.Println("test_category_page不是字符串类型!请检查!")
+			}
+		case k == "test_vod_index":
+			if num, ok := v.(int); ok {
+				test_vod_index = num
+			} else {
+				fmt.Println("test_vod_index不是int类型!请检查!")
+			}
+		case k == "test_vod_from_index":
+			if num, ok := v.(int); ok {
+				test_vod_from_index = num
+			} else {
+				fmt.Println("test_vod_from_index不是int类型!请检查!")
+			}
+
+		}
+	}
 }
 
 func main() {
-	ReadYaml()
+	InitConfig()
+	c := Go_HomeContent(extend, true, "a.py")
+	fmt.Println(c)
 }
